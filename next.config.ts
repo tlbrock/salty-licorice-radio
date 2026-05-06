abddd5252355450fba798449366d4ccdca027de0
tlbrock <43982385+tlbrock@users.noreply.github.com>
2026-05-06 10:52:21 -0400

    adjusting config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/salty-licorice-radio" : "",
 };
 
 export default nextConfig;
